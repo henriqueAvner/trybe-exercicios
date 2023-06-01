d1e1846b31e7b013bcc86799aa54e2780e369273
henriqueAvner <avnerhdpb@gmail.com>
2023-06-01 10:17:13 -0300

    Exercicio de maior número

---
+++ modulo1.2/secao-3-Introdução-a-JavaScript/dia-1-primeiros-passos-no-JS/Exercícios-alternativos/ex2.js
@@ -0,0 +1,12 @@
+let num1 = 456;
+let num2 = 1005;
+let num3 = 55;
+let maiorNum
+if (num1 > num2 && num1 > num3){
+    maiorNum = num1;
+}else if (num2 > num1 && num2 > num3){
+    maiorNum = num2;
+}else if(num3 > num1 && num3 > num2){
+    maiorNum = num3;
+}
+console.log('O maior número é: ' + maiorNum);
