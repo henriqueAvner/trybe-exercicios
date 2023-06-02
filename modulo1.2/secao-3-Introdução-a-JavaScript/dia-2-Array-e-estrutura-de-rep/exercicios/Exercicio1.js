1026c5abfcfaa6e205acf7503838a41675b4554c
henriqueAvner <avnerhdpb@gmail.com>
2023-06-01 22:41:43 -0300

    exercicio 1 percorrer a array

---
+++ modulo1.2/secao-3-Introdução-a-JavaScript/dia-2-Array-e-estrutura-de-rep/exercicios/Exercicio1.js
@@ -0,0 +1,8 @@
+const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let valoresNumbers;
+for (index = 0; index < numbers.length; index += 1){
+    valoresNumbers = numbers[index];
+    console.log(valoresNumbers)
+}
+
+//Percorra o array imprimindo todos os valores contidos nele com a função console.log().
