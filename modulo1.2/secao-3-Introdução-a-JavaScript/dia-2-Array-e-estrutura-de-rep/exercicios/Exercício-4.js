99cbfcaa767eb809b3404f42f553f95eb572ba35
henriqueAvner <avnerhdpb@gmail.com>
2023-06-03 18:25:21 -0300

    Exercicio 4 asteriscos

---
+++ modulo1.2/secao-3-Introdução-a-JavaScript/dia-2-Array-e-estrutura-de-rep/exercicios/Exercício-4.js
@@ -0,0 +1,9 @@
+let n = 7;
+let asterisco = '*';
+let espacoVazio = '';
+for(let index = 0; index < n; index +=1){
+    espacoVazio += asterisco;
+}
+for(let index = 0; index <n; index +=1){
+    console.log(espacoVazio);
+}
