fd03039772e455ba5e3c545cb031ffb5647e3cbb
henriqueAvner <avnerhdpb@gmail.com>
2023-06-14 08:49:20 -0300

    requisito 3

--- modulo1.2/secao-4-JS-DOM/dia-02-js-trabalho-com-elementos/script.js
+++ modulo1.2/secao-4-JS-DOM/dia-02-js-trabalho-com-elementos/script.js
@@ -2,3 +2,6 @@ const reqOne = document.getElementById('where-are-you');
 
 const reqTwo = reqOne.parentElement;
 reqTwo.style.color = 'green';
+
+const reqThree = reqOne.firstElementChild;
+reqThree.innerText = "Looking for wisdom";
