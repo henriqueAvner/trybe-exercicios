5221deccc3ab72b55846f75d47fbeedade7a9670
henriqueAvner <avnerhdpb@gmail.com>
2023-06-14 18:12:29 -0300

    Exercicios sobre manipulação de objeto

---
+++ modulo1.2/secao-3-Introdução-a-JavaScript/dia-04-objetos/Manipulando objetos 3/exercicio.js
@@ -0,0 +1,27 @@
+const school = {
+    lessons: [
+      {
+        course: 'Python',
+        students: 20,
+        professor: 'Carlos Patrício',
+        shift: 'Manhã',
+      },
+      {
+        course: 'Kotlin',
+        students: 10,
+        professor: 'Gabriel Oliva',
+        shift: 'Noite',
+      },
+      {
+        course: 'JavaScript',
+        students: 738,
+        professor: 'Gustavo Caetano',
+        shift: 'Tarde',
+      },
+      {
+        course: 'MongoDB',
+        students: 50,
+        shift: 'Noite',
+      },
+    ]
+  };
