738f8e2113152fe8ad5b6a5465d86c49c85cf9b5
henriqueAvner <avnerhdpb@gmail.com>
2023-06-14 09:01:43 -0300

    Adicionando requisitos finais

--- modulo1.2/secao-4-JS-DOM/dia-02-js-trabalho-com-elementos/script.js
+++ modulo1.2/secao-4-JS-DOM/dia-02-js-trabalho-com-elementos/script.js
@@ -5,3 +5,15 @@ reqTwo.style.color = 'green';
 
 const reqThree = reqOne.firstElementChild;
 reqThree.innerText = "Looking for wisdom";
+
+const reqFour = document.getElementById('parent').firstElementChild;
+
+const reqFive = reqOne.previousElementSibling;
+
+const reqSix = reqOne.nextSibling;
+
+const reqSeven = reqOne.nextElementSibling;
+
+const reqEight = document.getElementById('third-child');
+const reqFinal = reqEight.parentElement;
+
