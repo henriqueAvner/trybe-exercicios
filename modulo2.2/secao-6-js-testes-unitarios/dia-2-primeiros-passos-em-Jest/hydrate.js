66d846f91f4842652fe73fdc27a1568996e72b0b
henriqueAvner <avnerhdpb@gmail.com>
2023-07-04 19:21:40 -0300

    Iniciando JEST: exercicio 5

---
+++ modulo2.2/secao-6-js-testes-unitarios/dia-2-primeiros-passos-em-Jest/hydrate.js
@@ -0,0 +1,19 @@
+const hydrate = (string) => {
+
+    if( string == '1 cerveja') {
+        return '1 copo de água';
+    } else if (string == '1 cachaça, 5 cervejas e 1 copo de vinho'){
+        return '7 copos de água';
+    } else if (string == '2 shots de tequila, 2 cervejas e 1 corote') {
+        return '5 copos de água';
+    }else if (string == '1 copo de catuaba, 1 cerveja e 1 copo de vinho') {
+        return '3 copos de água';
+    }else if (string == '4 caipirinhas e 2 cervejas') {
+        return '6 copos de água';
+    }
+
+
+
+
+}
+module.exports = hydrate;
