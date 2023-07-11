f2c9d718771d73d4310f9676efef47471a63510e
henriqueAvner <avnerhdpb@gmail.com>
2023-07-11 14:27:03 -0300

    exercicio 5: org-biblioteca

--- modulo2.2/secao-7-higher-order-functions/dia-1-introdução/Exercicios-do-dia/org-biblioteca.js
+++ modulo2.2/secao-7-higher-order-functions/dia-1-introdução/Exercicios-do-dia/org-biblioteca.js
@@ -107,3 +107,9 @@ function everyoneWasBornOnSecXX() {
 }
 
 //
+
+const expectedResult5 = true;
+
+const someBookWasReleaseOnThe80s = () => {
+  return books.some((book) => book.releaseYear >= 1980 || book.releaseYear <= 1989);
+}
